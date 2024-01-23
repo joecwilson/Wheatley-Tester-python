f6ce249e4737b16566dfeded7e334ea7e6426be7
joseph <josephcwilson47@gmail.com>
2024-01-22 21:01:38 -0600

    Initial changes for timing

--- src/wheatley_tester/main.py
+++ src/wheatley_tester/main.py
@@ -5,25 +5,28 @@ import time
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum, auto
-from typing import Any, List, Optional, TextIO, Tuple
+from typing import List, Optional, TextIO, Tuple
 
 import chess
 import chess.pgn
 
-DEFAULT_TIME_SECONDS = 1
+DEFAULT_ENGINE_PATH = (
+    "/home/joseph/personal_projects/wheatley_bot/target/release/wheatley_bot"
+)
+DEFAULT_TIME_SECONDS = 2
 DEFAULT_TRIES = 200
 SLEEP_START_TIME = 0.05
 
 
 class GameOutcome(Enum):
-    WHITE_WIN_MATE = auto()
-    WHITE_WIN_TIME = auto()
-    WHITE_WIN_FOREFIT = auto()
-    BLACK_WIN_MATE = auto()
-    BLACK_WIN_TIME = auto()
-    BLACK_WIN_FOREFIT = auto()
-    DRAW = auto()
-    STALEMATE = auto()
+    WHITE_WIN_MATE = (auto(),)
+    WHITE_WIN_TIME = (auto(),)
+    WHITE_WIN_FOREFIT = (auto(),)
+    BLACK_WIN_MATE = (auto(),)
+    BLACK_WIN_TIME = (auto(),)
+    BLACK_WIN_FOREFIT = (auto(),)
+    DRAW = (auto(),)
+    STALEMATE = (auto(),)
 
     def is_white_win(self):
         return (
@@ -57,33 +60,8 @@ class GameResult:
     new_engine_loss: bool
     moves: List[str]
     opening_moves: List[str]
-    white_engine_name: str
-    black_engine_name: str
-
-
-class UCIType(Enum):
-    CHECK = auto()
-    SPIN = auto()
-    COMBO = auto()
-    BUTTON = auto()
-    STRING = auto()
-
-
-@dataclass
-class UCIOption:
-    name: str
-    uci_type: UCIType
-    default: Optional[Any] = None
-    minimum: Optional[Any] = None
-    maximum: Optional[Any] = None
-    var: Optional[List[Any]] = None
-
-
-@dataclass
-class UCIID:
-    name: str
-    author: str
-    options: List[UCIOption]
+    white_engine: str
+    black_engine: str
 
 
 def get_parser():
@@ -91,8 +69,8 @@ def get_parser():
         prog="Wheatley Bot Tester",
         description="Tests UCI compatable chess engines that pefer to lose",
     )
-    parser.add_argument("NewEngine")
-    parser.add_argument("OldEngine")
+    parser.add_argument("NewEngine", default=DEFAULT_ENGINE_PATH)
+    parser.add_argument("OldEngine", default=DEFAULT_ENGINE_PATH)
     parser.add_argument("--time-limit", type=int, default=DEFAULT_TIME_SECONDS)
     return parser
 
@@ -108,22 +86,7 @@ def main():
         print(f"Working on opening {counter}")
         match_results.extend(run_match(opening, args.NewEngine, args.OldEngine))
         counter += 1
-    # match_results.extend(run_match(openings[0], args.NewEngine, args.OldEngine))
     losses, wins, draws, forefits = num_losses_wins_draws_forefits(match_results)
-
-    # Lets parse the name
-    new_engine = subprocess.Popen(
-        args.NewEngine, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
-    )
-    old_engine = subprocess.Popen(
-        args.OldEngine, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
-    )
-    old_engine_id = boot_engine(old_engine)
-    new_engine_id = boot_engine(new_engine)
-
-    new_engine.terminate()
-    old_engine.terminate()
-
     passed_run = True
     if forefits > 0:
         passed_run = False
@@ -131,8 +94,8 @@ def main():
         passed_run = False
     write_games_to_disk(
         match_results,
-        old_engine_id.name,
-        new_engine_id.name,
+        args.OldEngine,
+        args.NewEngine,
         passed_run,
         losses,
         wins,
@@ -155,18 +118,12 @@ def parse_opening_book() -> List[List[str]]:
 def run_match(
     opening_moves: List[str], new_engine_path: str, old_engine_path: str
 ) -> Tuple[GameResult, GameResult]:
-    (
-        new_as_white_outcome,
-        new_as_white_moves,
-        new_as_white_white_id,
-        new_as_white_black_id,
-    ) = run_game(opening_moves, new_engine_path, old_engine_path)
-    (
-        new_as_black_outcome,
-        new_as_black_moves,
-        new_as_black_white_id,
-        new_as_black_black_id,
-    ) = run_game(opening_moves, old_engine_path, new_engine_path)
+    new_as_white_outcome, new_as_white_moves = run_game(
+        opening_moves, new_engine_path, old_engine_path
+    )
+    new_as_black_outcome, new_as_black_moves = run_game(
+        opening_moves, old_engine_path, new_engine_path
+    )
 
     new_as_white_result = GameResult(
         is_drawn=new_as_white_outcome.is_drawn(),
@@ -175,8 +132,6 @@ def run_match(
         new_engine_loss=new_as_white_outcome.is_black_win(),
         moves=new_as_white_moves,
         opening_moves=opening_moves,
-        white_engine_name=new_as_white_white_id.name,
-        black_engine_name=new_as_white_black_id.name,
     )
     new_as_black_result = GameResult(
         is_drawn=new_as_black_outcome.is_drawn(),
@@ -185,23 +140,23 @@ def run_match(
         new_engine_loss=new_as_black_outcome.is_white_win(),
         moves=new_as_black_moves,
         opening_moves=opening_moves,
-        white_engine_name=new_as_black_white_id.name,
-        black_engine_name=new_as_black_black_id.name,
     )
     return (new_as_white_result, new_as_black_result)
 
 
 def run_game(
     opening_moves: List[str], white_engine_path: str, black_engine_path: str
-) -> Tuple[GameOutcome, List[str], UCIID, UCIID]:
+) -> Tuple[GameOutcome, List[str]]:
     white_engine = subprocess.Popen(
         white_engine_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
     )
     black_engine = subprocess.Popen(
         black_engine_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
     )
-    white_engine_id = boot_engine(white_engine)
-    black_engine_id = boot_engine(black_engine)
+    boot_engine(white_engine)
+    boot_engine(black_engine)
+    white_time = DEFAULT_TIME_SECONDS * 1000
+    black_time = DEFAULT_TIME_SECONDS * 1000
 
     board = chess.Board()
     for move in opening_moves:
@@ -209,110 +164,87 @@ def run_game(
 
     moves = copy.deepcopy(opening_moves)
     while not board.is_game_over(claim_draw=True):
+        start_time = time.time_ns()
         if board.turn == chess.WHITE:
-            bestmove = get_bestmove(moves, white_engine)
+            bestmove = get_bestmove(moves, white_engine, white_time, black_time)
         else:
-            bestmove = get_bestmove(moves, black_engine)
+            bestmove = get_bestmove(moves, black_engine, white_time, black_time)
+        end_time = time.time_ns()
+        elapsed_time_ns = end_time - start_time
+        elapsed_time_ms = (elapsed_time_ns // 1_000_000) + 1
+        if board.turn == chess.WHITE:
+            white_time -= elapsed_time_ms
+        else:
+            black_time -= elapsed_time_ms
         moves.append(bestmove)
         board.push_uci(bestmove)
+        if white_time < 0:
+            # Fail out of time
+            white_engine.terminate()
+            black_engine.terminate()
+            return (GameOutcome.BLACK_WIN_TIME, moves)
+        if black_time < 0:
+            white_engine.terminate()
+            black_engine.terminate()
+            return (GameOutcome.WHITE_WIN_TIME, moves)
     white_engine.terminate()
     black_engine.terminate()
     outcome = board.outcome(claim_draw=True)
     assert outcome is not None
     if outcome.termination == chess.Termination.STALEMATE:
-        return (GameOutcome.STALEMATE, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.STALEMATE, moves)
     elif outcome.termination == chess.Termination.INSUFFICIENT_MATERIAL:
-        return (GameOutcome.DRAW, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.DRAW, moves)
     elif outcome.termination == chess.Termination.SEVENTYFIVE_MOVES:
-        return (GameOutcome.DRAW, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.DRAW, moves)
     elif outcome.termination == chess.Termination.FIVEFOLD_REPETITION:
-        return (GameOutcome.DRAW, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.DRAW, moves)
     elif outcome.termination == chess.Termination.FIFTY_MOVES:
-        return (GameOutcome.DRAW, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.DRAW, moves)
     elif outcome.termination == chess.Termination.THREEFOLD_REPETITION:
-        return (GameOutcome.DRAW, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.DRAW, moves)
     assert outcome.winner is not None
     if outcome.winner == chess.WHITE:
-        return (GameOutcome.WHITE_WIN_MATE, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.WHITE_WIN_MATE, moves)
     else:
-        return (GameOutcome.BLACK_WIN_MATE, moves, white_engine_id, black_engine_id)
+        return (GameOutcome.BLACK_WIN_MATE, moves)
 
 
-def boot_engine(engine_process: subprocess.Popen) -> UCIID:
+def boot_engine(engine_process: subprocess.Popen):
+    blocking_readline(engine_process)
     assert engine_process.stdout is not None
     assert engine_process.stdin is not None
-    blocking_readline(engine_process)
     engine_process.stdin.write("uci\n")
     engine_process.stdin.flush()
-
-    line = blocking_readline(engine_process)
-    lines = [line]
-    while line != "uciok\n":
-        line = blocking_readline(engine_process)
-        lines.append(line)
-
+    blocking_readline(engine_process)
+    blocking_readline(engine_process)
+    blocking_readline(engine_process)
+    blocking_readline(engine_process)
     engine_process.stdin.write("isready\n")
     engine_process.stdin.flush()
     blocking_readline(engine_process)
-    return parse_options(lines)
-
-
-def parse_options(lines: List[str]) -> UCIID:
-    name = ""
-    author = ""
-    options = []
-    for line in lines:
-        line_split = line.split()
-        if line_split[0] == "id":
-            if line_split[1] == "name":
-                name = " ".join(line_split[2:])
-            if line_split[1] == "author":
-                author = " ".join(line_split[2:])
-        elif line_split[0] == "option":
-            # Assume that it is name type default
-            option_name = line_split[2]
-            option_type_str = line_split[4]
-            option_type = None
-            match option_type_str:
-                case "check":
-                    option_type = UCIType.CHECK
-                case "spin":
-                    option_type = UCIType.SPIN
-                case "combo":
-                    option_type = UCIType.COMBO
-                case "button":
-                    option_type = UCIType.BUTTON
-                case "string":
-                    option_type = UCIType.STRING
-                case _:
-                    raise AssertionError("Unexpected type")
-            option_default = line_split[6]
-            option = UCIOption(
-                name=option_name, uci_type=option_type, default=option_default
-            )
-            options.append(option)
-    return UCIID(name=name, author=author, options=options)
 
 
 def get_bestmove(
     moves: List[str],
     engine_process: subprocess.Popen,
-    w_time: Optional[float] = None,
-    b_time: Optional[float] = None,
+    w_time: Optional[int] = None,
+    b_time: Optional[int] = None,
 ) -> str:
     assert engine_process.stdout is not None
     assert engine_process.stdin is not None
     engine_process.stdin.write(get_position(moves))
     engine_process.stdin.flush()
     if w_time is not None and b_time is not None:
-        engine_process.stdin.write("go infinite\n")  # TODO: Add time tracking
+        engine_process.stdin.write(f"go wtime {w_time} btime {b_time} \n") 
         engine_process.stdin.flush()
     else:
-        engine_process.stdin.write("go infinite\n")
+        engine_process.stdin.write("go \n")
         engine_process.stdin.flush()
     best_move_line = blocking_readline(
         engine_process
     )  # TODO: Handle more than 1 line of output
+    # print(f" Bestmove line = {best_move_line}")
     best_move_split = best_move_line.split()
     assert best_move_split
     return best_move_split[1]
@@ -326,7 +258,7 @@ def blocking_readline(engine_process: subprocess.Popen, should_print: bool = Fal
         line = engine_process.stdout.readline()
         counter += 1
         if line is None:
-            time.sleep(SLEEP_START_TIME * (counter**2))
+            time.sleep(SLEEP_START_TIME * (counter**1.5))
     if line is None:
         raise AssertionError("Number of tries failed")
     if should_print:
@@ -395,11 +327,11 @@ def write_game_to_disk(game: GameResult, file: TextIO):
     file.write(f"Moves = {moves_as_string}\n")
 
     pgn_game = chess.pgn.Game()
+    pgn_game.headers["White"] = "White Engine" 
+    pgn_game.headers["Black"] = "Black Engine" 
     node = pgn_game.add_variation(chess.Move.from_uci(game.moves[0]))
     opening_end = len(game.opening_moves) - 2
     exporter = chess.pgn.FileExporter(file)
-    pgn_game.headers["White"] = game.white_engine_name
-    pgn_game.headers["Black"] = game.black_engine_name
     for idx, move in enumerate(game.moves[1:]):
         node = node.add_variation(chess.Move.from_uci(move))
         if idx == opening_end:
